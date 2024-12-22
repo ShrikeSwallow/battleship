ae2f6a3e8b884a7476f8065a0feeffe6442f44fd
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-22 11:50:40 +0000

    Implement very simple initialize function

--- src/Display.js
+++ src/Display.js
@@ -1,3 +1,14 @@
 export default class Display {
   constructor() {}
+  initialize = () => {
+    const body = document.body;
+    const header = document.createElement("header");
+    const main = document.createElement("main");
+
+    const title = document.createElement("h1");
+    title.textContent = "Battleships";
+    header.appendChild(title);
+    body.appendChild(header);
+    body.appendChild(main);
+  };
 }
